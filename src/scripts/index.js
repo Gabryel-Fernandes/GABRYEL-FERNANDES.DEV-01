13b3cf12ad5eca512adf05e3760694def912ffb3
Gabryel Fernandes <gabryelfernandes67@gmail.com>
2024-07-09 19:08:19 -0300

    refatorcao do esqueleto do projeto

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,24 @@
-let elementsbar = document.querySelector('.cards-bar');
-let elementSlider = elementsbar.querySelector('.slider');
+/*----------funcionamento dp scroll lateral do projects----------*/
+let elementsbar = document.querySelector('.slider-bar');
+let elementSlider = elementsbar.querySelector('.slider-itens');
 let elementsdaughters = Array.from(elementSlider.children);
 
-elementsdaughters.forEach((item) =>{
-    let duplicateElement =item.cloneNode(true);
+elementsdaughters.forEach((item) => {
+    let duplicateElement = item.cloneNode(true);
 
     elementSlider.appendChild(duplicateElement);
     console.log(item);
 })
 
-// console.log(elementsdaughters);
+/*----------funcionamento da flecha button-----------*/
+
+let excuteButton = document.querySelector('.contact');
+let moveArrow = document.querySelector('.arrow');
+
+excuteButton.addEventListener('mouseenter', () =>{
+    moveArrow.classList.add('active-arrow')
+})
+
+excuteButton.addEventListener('mouseout', () =>{
+    moveArrow.classList.remove('active-arrow')
+})
