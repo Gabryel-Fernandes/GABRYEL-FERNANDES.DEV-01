28039a7f2d7cebb576e0c2c75b61ea1e667c5601
Gabryel Fernandes <gabryelfernandes67@gmail.com>
2024-07-19 18:26:48 -0300

    interatividade com os cartoes de skill adicionados

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,11 +14,11 @@ elementsdaughters.forEach((item) => {
 let excuteButton = document.querySelector('.contact');
 let moveArrow = document.querySelector('.arrow');
 
-excuteButton.addEventListener('mouseenter', () =>{
+excuteButton.addEventListener('mouseenter', () => {
     moveArrow.classList.add('active-arrow')
 })
 
-excuteButton.addEventListener('mouseout', () =>{
+excuteButton.addEventListener('mouseout', () => {
     moveArrow.classList.remove('active-arrow')
 })
 
@@ -29,7 +29,7 @@ const navigationbar = document.querySelector('.navigation')
 const iconsSocials = document.querySelector('.social-links')
 const iconsSections = document.querySelector('.sections')
 
-buttonHamburguerNav.addEventListener('click', () =>{
+buttonHamburguerNav.addEventListener('click', () => {
     activeIconsNav();
     removeOpacity();
 })
@@ -39,8 +39,83 @@ function activeIconsNav() {
     buttonHamburguerNav.classList.toggle('background-hamburguer-active');
 }
 
-function removeOpacity(){
+function removeOpacity() {
     iconsSocials.style.opacity = '100%';
     iconsSections.style.opacity = '100%';
-    
+
 }
+
+/*-----------funcionamento das cartas-------------*/
+
+
+const cards = document.querySelectorAll('.magic-card')
+const shadowModal = document.querySelector('.container-modal')
+const allContent = document.querySelector('.all-content')
+
+const modalHtml = document.querySelector('.modal-html')
+const modalCard = document.querySelector('.modal-card')
+const modalCss = document.querySelector('.modal-css')
+const modalJs = document.querySelector('.modal-js')
+const modalGit = document.querySelector('.modal-git')
+const modalDesign = document.querySelector('.modal-design')
+
+const closeModalCards = document.querySelectorAll('.close-modal')
+
+
+closeModalCards.forEach((closer) => {
+    closer.addEventListener('click', ()=>{
+        allContent.style.overflow = 'visible';
+        shadowModal.style.display = 'none';
+        modalHtml.style.display = 'none';
+        modalCard.style.display = 'none';
+        modalCss.style.display = 'none';
+        modalJs.style.display = 'none';
+        modalGit.style.display = 'none';
+        modalDesign.style.display = 'none';
+    })
+})
+
+
+cards.forEach(() => {
+    openAndCloseModal();
+});
+
+
+function openAndCloseModal() {
+    cards[0].addEventListener('click', () => {
+        shadowModal.style.display = 'block';
+        modalHtml.style.display = 'flex';
+        allContent.style.overflow = 'hidden';
+    });
+
+    cards[1].addEventListener('click', () => {
+        shadowModal.style.display = 'block';
+        modalCard.style.display = 'flex';
+        allContent.style.overflow = 'hidden';
+    });
+
+    cards[2].addEventListener('click', () => {
+        shadowModal.style.display = 'block';
+        modalCss.style.display = 'flex';
+        allContent.style.overflow = 'hidden';
+    });
+
+    cards[3].addEventListener('click', () => {
+        shadowModal.style.display = 'block';
+        modalJs.style.display = 'flex';
+        allContent.style.overflow = 'hidden';
+    });
+
+    cards[4].addEventListener('click', () => {
+        shadowModal.style.display = 'block';
+        modalGit.style.display = 'flex';
+        allContent.style.overflow = 'hidden';
+    });
+
+    cards[5].addEventListener('click', () => {
+        shadowModal.style.display = 'block';
+        modalDesign.style.display = 'flex';
+        allContent.style.overflow = 'hidden';
+    });
+}
+
